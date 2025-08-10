7410b051e89b7ad2ff09a7b19642d6bf82578638
Sujan Bhatta <sujanbhatta0629@gmail.com>
2025-08-10 10:18:48 +0000

    images change

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -8,13 +8,13 @@ export const imageUrls = {
 
   // Page Backgrounds
   aboutBackground: 'https://images.unsplash.com/photo-1506806732259-39c2d0268443?auto=format&fit=crop&w=1920&h=1080',
-  timelineBackground: 'https://images.unsplash.com/photo-1533139502658-0198f920d8e8?auto=format&fit=crop&w=1920&h=1080',
-  projectsBackground: 'https://images.unsplash.com/photo-1465433045946-ba6506ce5a59?auto=format&fit=crop&w=1920&h=1080',
+  timelineBackground: 'https://caddcentre.com/blog/wp-content/uploads/2022/02/Women-Civil-1-1024x684.jpeg',
+  projectsBackground: 'https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/3GgkKundG6WGpPHOPFBdDq/7cba9177de38af6c6fd9f30b7d377a91/Construction-workers-and-architect-looking-at-blueprints-on-construction-site-514311930_5413x3609__1_.jpeg?w=1500&h=680&q=60&fit=fill&f=faces&fm=jpg&fl=progressive&auto=format%2Ccompress&dpr=1&w=1000',
   researchBackground: 'https://images.unsplash.com/photo-1581091226033-d5c48150dbaa?auto=format&fit=crop&w=1920&h=1080',
   blogBackground: 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?auto=format&fit=crop&w=1920&h=1080',
-  certificationsBackground: 'https://images.unsplash.com/photo-1553877522-43269d4ea984?auto=format&fit=crop&w=1920&h=1080',
+  certificationsBackground: 'https://media.licdn.com/dms/image/v2/D4D12AQFrJYmW23UW7w/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1654286931189?e=2147483647&v=beta&t=YvEaBCFjBeW7uy5RManuaOES6l-3F3ZxaRJVaAu_3gs',
   qaBackground: 'https://images.unsplash.com/photo-1551650975-87deedd944c3?auto=format&fit=crop&w=1920&h=1080',
-  contactBackground: 'https://images.unsplash.com/photo-1497366754035-f200968a6e72?auto=format&fit=crop&w=1920&h=1080',
+  contactBackground: 'https://img.freepik.com/premium-photo/employee-man-hand-press-number-telephone-contact-with-someone_42708-11.jpg?semt=ais_hybrid&w=740&q=80',
   
   // Section-specific images
   journalArticles: 'https://images.unsplash.com/photo-1495640388908-05fa85288e61?auto=format&fit=crop&w=600&h=400',
