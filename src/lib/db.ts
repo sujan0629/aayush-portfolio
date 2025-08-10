7a85fa6646864cb99f81ffecb102f987f11d00a8
Sujan Bhatta <sujanbhatta0629@gmail.com>
2025-08-10 11:14:01 +0000

    8th commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,28 +6,17 @@ if (!MONGO_URL) {
   throw new Error('Please define the MONGO_URL environment variable inside .env');
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
+/**
+ * Connects to the MongoDB database.
+ * This function is designed to work in serverless environments.
+ */
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+  try {
+    await mongoose.connect(MONGO_URL);
+  } catch (e) {
+    console.error('Connection error', e);
+    throw new Error('Connection error');
   }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
 export default dbConnect;
