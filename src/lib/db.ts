6d5b65358c8acd10107a0383229f76575cff542f
Sujan Bhatta <sujanbhatta0629@gmail.com>
2025-08-10 11:15:03 +0000

    9th commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,16 +6,12 @@ if (!MONGO_URL) {
   throw new Error('Please define the MONGO_URL environment variable inside .env');
 }
 
-/**
- * Connects to the MongoDB database.
- * This function is designed to work in serverless environments.
- */
 async function dbConnect() {
   try {
     await mongoose.connect(MONGO_URL);
   } catch (e) {
     console.error('Connection error', e);
-    throw new Error('Connection error');
+    throw new Error('Could not connect to MongoDB');
   }
 }
 
